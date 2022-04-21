19fee4723a29cb15e7abd537b72d8f33447b7413
Mariusz Bal <developer.m.info@gmail.com>
2022-04-21 13:01:47 +0200

    Initial commit

---
+++ src/main/java/org/wildhamsters/battleships/BattleshipsApplication.java
@@ -0,0 +1,13 @@
+package org.wildhamsters.battleships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wildhamsters/battleships/BattleshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wildhamsters.battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
