a4ca80f92e8937cc8b1c1aaeef316236fb7f8924
d.żebracki <d.zebracki@gmail.com>
2022-04-21 16:53:01 +0200

    Rename Dimension

--- src/main/java/org/wildhamsters/battleships/Dimensions.java
+++
@@ -1,8 +0,0 @@
-package org.wildhamsters.battleships;
-
-record Dimensions(int min, int max) {
-    
-    boolean isWithinDimensions(int value) {
-        return value >= min && value <= max;
-    }
-}
