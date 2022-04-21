b79baaa2ef7ebe8924d93edb39acca1d66780cce
KamKoz92 <kamilkoziol92@gmail.com>
2022-04-21 17:42:08 +0200

    feat: shoot verificator

--- src/main/java/org/wildhamsters/battleships/BoardDimension.java
+++ src/main/java/org/wildhamsters/battleships/BoardDimension.java
@@ -1,7 +1,6 @@
 package org.wildhamsters.battleships;
 
 record BoardDimension(int min, int max) {
-    
     boolean isWithinDimension(int value) {
         return value >= min && value <= max;
     }
