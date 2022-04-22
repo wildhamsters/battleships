8174cb38c0b54bc999285f6e03ccd89a284860e2
d.żebracki <d.zebracki@gmail.com>
2022-04-22 11:37:42 +0200

    feat: Add demo handling ona back-end

--- src/main/java/org/wildhamsters/battleships/DefaultBoard.java
+++ src/main/java/org/wildhamsters/battleships/DefaultBoard.java
@@ -1,16 +1,26 @@
 package org.wildhamsters.battleships;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 /**
  * @author Dominik Żebracki
  */
+@Component
 class DefaultBoard implements Board {
 
     private final ArrayList<FieldState> board;
 
     DefaultBoard(ArrayList<FieldState> board) {
         this.board = board;
+    }
+
+    DefaultBoard() {
+        ArrayList<FieldState> list = new ArrayList<>();
+        IntStream.range(0, 25).forEach(x -> list.add(FieldState.WATER));
+        this.board = list;
     }
 
     @Override
@@ -21,6 +31,11 @@ class DefaultBoard implements Board {
     @Override
     public void setField(FieldState fieldState, int position) {
         board.set(position, fieldState);
+    }
+
+    @Override
+    public void clearBoard() {
+        IntStream.range(0, board.size()).forEach(i -> board.set(i, FieldState.WATER));
     }
 
     @Override
