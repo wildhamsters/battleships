abcf466de22d15699bc3095d2b365b6a752ee583
KamKoz92 <51924117+KamKoz92@users.noreply.github.com>
2022-04-28 20:03:50 +0200

    Delete commented code.

--- src/main/java/org/wildhamsters/battleships/GameService.java
+++ src/main/java/org/wildhamsters/battleships/GameService.java
@@ -13,39 +13,26 @@ import org.wildhamsters.battleships.play.IllegalShotException;
 public
 class GameService {
 
-//    private final ShotVerifier shotVerifier;
     private final Board board;
     private final Fleet fleet;
 
     public GameService(Board board) {
         this.board = board;
-//        shotVerifier = new ShotVerifier();
         fleet = new Fleet();
-//        placeShipsOnBoard();
     }
 
     public FieldState verifyShot(int position) throws IllegalShotException {
-//        FieldState state = shotVerifier.verifyShot(position, board);
         fleet.makeShot(position);
-//        updateFieldState(state, position);
         return null;
     }
 
     void resetGameStatus() {
         board.clearBoard();
-//        fleet.resetAllShipsToUntouched();
-//        placeShipsOnBoard();
     }
 
     public boolean isRoundFinished() {
         return fleet.checkIfAllShipsSunk();
     }
-
-//    private void placeShipsOnBoard() {
-//        for(Integer position : fleet.allTakenFields.getAllFieldsInOneList()) {
-//            board.setField(FieldState.INTACT_SHIP, position);
-//        }
-//    }
 
     private void updateFieldState(FieldState newState, int position) {
         board.setField(newState, position);
