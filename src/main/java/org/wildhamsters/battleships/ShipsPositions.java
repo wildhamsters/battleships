0a5ffd8e3c6a5e52d022265a2c06ba92109a3c7c
d.żebracki <d.zebracki@gmail.com>
2022-04-28 12:21:45 +0200

    feat: Add toString method
    Will be needed in testing

--- src/main/java/org/wildhamsters/battleships/ShipsPositions.java
+++ src/main/java/org/wildhamsters/battleships/ShipsPositions.java
@@ -22,4 +22,9 @@ class ShipsPositions {
     List<ShipPosition> getAllShipsPositions() {
         return shipsPosition;
     }
+
+    @Override
+    public String toString() {
+        return shipsPosition.toString();
+    }
 }
