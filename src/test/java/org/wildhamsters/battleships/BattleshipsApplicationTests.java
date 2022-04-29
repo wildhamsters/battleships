81a297c5a66fdf55823e06c000e1dbaafee2e7bb
Kevin Nowak <nowakk96@icloud.com>
2022-04-29 11:01:03 +0200

    Added comment to BattleshipApplicationTest

--- src/test/java/org/wildhamsters/battleships/BattleshipsApplicationTests.java
+++ src/test/java/org/wildhamsters/battleships/BattleshipsApplicationTests.java
@@ -1,12 +1,17 @@
 package org.wildhamsters.battleships;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.annotations.Test;
+
 
 @SpringBootTest
 class BattleshipsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// empty test that would fail if our Spring configuration does not load correctly
 	}
 }
