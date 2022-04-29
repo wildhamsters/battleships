4d0877e7939a9d6bc06f0f391297fda4586ad8a1
d.żebracki <d.zebracki@gmail.com>
2022-04-29 09:54:33 +0200

    fix: Add missing id field in GameSettings

--- src/main/java/org/wildhamsters/battleships/configuration/GameConfigurer.java
+++ src/main/java/org/wildhamsters/battleships/configuration/GameConfigurer.java
@@ -15,19 +15,23 @@ public class GameConfigurer {
     public GameSettings createConfiguration(List<Integer> shipSizesToBePlaced,
                                             int boardHeight,
                                             int boardWidth,
-                                            List<String> playersNames) {
+                                            List<String> playersNames,
+                                            List<String> playersIds) {
         var shipPlacementConfigurer = new ShipPlacementConfigurer(boardHeight, boardWidth, new Random());
         return new GameSettings(IntStream.range(0, playersNames.size())
                 .boxed()
-                .map(i -> createPlayerSettings(shipPlacementConfigurer, playersNames.get(i), shipSizesToBePlaced))
+                .map(i -> createPlayerSettings(shipPlacementConfigurer, playersIds.get(i),
+                        playersNames.get(i), shipSizesToBePlaced))
                 .toList());
     }
 
     private GameSettings.PlayerSettings createPlayerSettings(ShipPlacementConfigurer shipPlacementConfigurer,
+                                                             String id,
                                                              String playerName,
                                                              List<Integer> shipSizesToBePlaced) {
         var shipPositions = shipPlacementConfigurer.placeShips(shipSizesToBePlaced);
-        return new GameSettings.PlayerSettings(playerName,
+        return new GameSettings.PlayerSettings(id,
+                playerName,
                 Board.create(shipPositions),
                 new Fleet(shipPositions));
     }
