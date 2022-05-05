ea49642b9e19480614cbc5205b7e7be28b6bdd1d
Piotr Chowaniec <chowaniec.piotr@poczta.fm>
2022-05-05 10:02:46 +0200

    Fix GameRoomTest bug

--- src/test/java/org/wildhamsters/battleships/play/GameRoomTest.java
+++ src/test/java/org/wildhamsters/battleships/play/GameRoomTest.java
@@ -32,7 +32,7 @@ public class GameRoomTest {
         return new Object[][] {
                 {prepareGameRoom(), prepareResult(0, FieldState.MISSED_SHOT, "2", "Player Two", "1"), 0},
                 {prepareGameRoom(), prepareResult(1, FieldState.ACCURATE_SHOT, "1", "Player One", "2"), 1},
-                {prepareGameRoom(), prepareResult(11, FieldState.ACCURATE_SHOT, "1", "Player One", "2"), 11},
+                {prepareGameRoom(), prepareResult(43, FieldState.ACCURATE_SHOT, "1", "Player One", "2"), 43},
                 {prepareGameRoom(), prepareResult(14, FieldState.MISSED_SHOT, "2", "Player Two", "1"), 14},
                 {prepareGameRoom(), prepareResult(23, FieldState.MISSED_SHOT, "2", "Player Two", "1"), 23},
                 {prepareGameRoom(), prepareResult(24, FieldState.MISSED_SHOT, "2", "Player Two", "1"), 24},
@@ -49,7 +49,8 @@ public class GameRoomTest {
         Board board = Board.create();
         board.setField(FieldState.INTACT_SHIP, 1);
         board.setField(FieldState.INTACT_SHIP, 2);
-        board.setField(FieldState.INTACT_SHIP, 11);
+        board.setField(FieldState.INTACT_SHIP, 43);
+        board.setField(FieldState.INTACT_SHIP, 44);
         return board;
     }
 
@@ -60,7 +61,7 @@ public class GameRoomTest {
     private Fleet prepareFleet() {
         List<ShipPosition> ships = new ArrayList<>() {{
             add(new ShipPosition(List.of(1, 2)));
-            add(new ShipPosition(List.of(11)));
+            add(new ShipPosition(List.of(43, 44)));
         }};
         return new Fleet(new ShipsPositions(ships));
     }
