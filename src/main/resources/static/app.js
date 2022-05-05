1be629e94a046ed873a79bc62d61c043e7677d11
KamKoz92 <kamilkoziol92@gmail.com>
2022-05-05 15:00:42 +0200

    app.js accept multiple fields at once

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     new Audio('audio/opener.wav').play();
 };
 
@@ -11,11 +11,11 @@ var width = 10;
 var height = 10;
 var roomId;
 
-var FIELD_STATE  = {
-	WATER: "WATER",
-	MISSED_SHOT: "MISSED_SHOT",
-	ACCURATE_SHOT: "ACCURATE_SHOT",
-	INTACT_SHIP: "INTACT_SHIP"
+var FIELD_STATE = {
+    WATER: "WATER",
+    MISSED_SHOT: "MISSED_SHOT",
+    ACCURATE_SHOT: "ACCURATE_SHOT",
+    INTACT_SHIP: "INTACT_SHIP"
 };
 
 var EVENT = {
@@ -24,95 +24,102 @@ var EVENT = {
 };
 
 function createPlayerBoard() {
-	var table = document.getElementById('playerBoard');
+    var table = document.getElementById('playerBoard');
 
-	for(var i=0; i<height; i++) {
-		var row = document.createElement('tr');
-			for(var j = 0; j<width; j++) {
-				var cell = document.createElement('td');
-				cell.id = 'cell_' + (i * width + j);
-				cell.innerHTML=(i * width + j);
-				cell.className="water";
-				cell.onclick=function(cell) {
-				    //sendName(extractCellNumber(this)); //todo disabled to disallow player's board clicking
-				}
-				cell.onmouseenter = function(){
-                    //hoverMany(extractCellNumber(this), 4, "H");
-                };
-                cell.onmouseleave = function(){
-                    //unhoverMany(extractCellNumber(this), 4, "H");
-                };
-				row.append(cell);
-			}
-			table.append(row);
-	}
-}
-
-function createOpponentBoard() {
-	var table = document.getElementById('opponentBoard');
-
-	for(var i=0; i<height; i++) {
-		var row = document.createElement('tr');
-			for(var j = 0; j<width; j++) {
-				var cell = document.createElement('td');
-				cell.id = 'ocell_' + (i * width + j);
-				cell.innerHTML=(i * width + j);
-				cell.className="water";
-				cell.onclick=function(cell) {
-				    sendName(extractCellNumber(this));
-				}
-				row.append(cell);
-			}
-			table.append(row);
-	}
-}
-
-function handleReturnedFieldType(returnedFieldType, cell, player) {
-	var id = "cell_" +  cell;
-	if(player)
-	    id = "o" + id;
-	switch(returnedFieldType) {
-    	case FIELD_STATE.MISSED_SHOT:
-    		changeDOMClassName(id, "missed");
-    		new Audio('audio/water.wav').play();
-    		break;
-    	case FIELD_STATE.WATER:
-    		changeDOMClassName(id, "water");
-    		new Audio('audio/confirm.wav').play();
-    		break;
-    	case FIELD_STATE.ACCURATE_SHOT:
-    		changeDOMClassName(id, "accurate");
-    		document.getElementById(id).innerHTML="&#128163";
-    		new Audio('audio/explosion.wav').play();
-    		break;
-    	case FIELD_STATE.INTACT_SHIP:
-    		changeDOMClassName(id, "intact");
-    		break;
+    for (var i = 0; i < height; i++) {
+        var row = document.createElement('tr');
+        for (var j = 0; j < width; j++) {
+            var cell = document.createElement('td');
+            cell.id = 'cell_' + (i * width + j);
+            cell.innerHTML = (i * width + j);
+            cell.className = "water";
+            cell.onclick = function (cell) {
+                //sendName(extractCellNumber(this)); //todo disabled to disallow player's board clicking
+            }
+            cell.onmouseenter = function () {
+                //hoverMany(extractCellNumber(this), 4, "H");
+            };
+            cell.onmouseleave = function () {
+                //unhoverMany(extractCellNumber(this), 4, "H");
+            };
+            row.append(cell);
+        }
+        table.append(row);
     }
 }
 
+function createOpponentBoard() {
+    var table = document.getElementById('opponentBoard');
+
+    for (var i = 0; i < height; i++) {
+        var row = document.createElement('tr');
+        for (var j = 0; j < width; j++) {
+            var cell = document.createElement('td');
+            cell.id = 'ocell_' + (i * width + j);
+            cell.innerHTML = (i * width + j);
+            cell.className = "water";
+            cell.onclick = function (cell) {
+                sendName(extractCellNumber(this));
+            }
+            row.append(cell);
+        }
+        table.append(row);
+    }
+}
+
+function handleReturnedFieldType(returnedFieldType, cell, player) {
+    var id = "cell_" + cell;
+    if (player)
+        id = "o" + id;
+    switch (returnedFieldType) {
+        case FIELD_STATE.MISSED_SHOT:
+            changeDOMClassName(id, "missed");
+            new Audio('audio/water.wav').play();
+            break;
+        case FIELD_STATE.WATER:
+            changeDOMClassName(id, "water");
+            new Audio('audio/confirm.wav').play();
+            break;
+        case FIELD_STATE.ACCURATE_SHOT:
+            changeDOMClassName(id, "accurate");
+            document.getElementById(id).innerHTML = "&#128163";
+            new Audio('audio/explosion.wav').play();
+            break;
+        case FIELD_STATE.INTACT_SHIP:
+            changeDOMClassName(id, "intact");
+            break;
+    }
+}
+function handleShipShink(cells, player) {
+
+}
+
+
+
+
+
 function extractCellNumber(cell) {
-    if(cell.id.startsWith("ocell"))
+    if (cell.id.startsWith("ocell"))
         return cell.id.replace("ocell_", "");
-	return cell.id.replace("cell_", "");
+    return cell.id.replace("cell_", "");
 }
 
 function badRequest() {
-	new Audio('audio/alert.wav').play();
+    new Audio('audio/alert.wav').play();
 }
 
 function hoverMany(startId, cellsCount, direction) {//todo make direction
-    var id = "cell_" +  startId;
-    for(var i=0; i<cellsCount; i++) {
-        document.getElementById("cell_" + (parseInt(startId) + i)).className='intact';
+    var id = "cell_" + startId;
+    for (var i = 0; i < cellsCount; i++) {
+        document.getElementById("cell_" + (parseInt(startId) + i)).className = 'intact';
     }
 }
 
 function unhoverMany(startId, cellsCount, direction) {//todo make direction
-    var id = "cell_" +  startId;
-    for(var i=0; i<cellsCount; i++) {
+    var id = "cell_" + startId;
+    for (var i = 0; i < cellsCount; i++) {
         var number = startId + i;
-        document.getElementById("cell_" + (parseInt(startId) + i)).className='water';
+        document.getElementById("cell_" + (parseInt(startId) + i)).className = 'water';
     }
 }
 
@@ -129,7 +136,7 @@ function lose() {
 }
 
 function changeDOMClassName(elementId, className) {
-	document.getElementById(elementId).className = className;
+    document.getElementById(elementId).className = className;
 }
 
 function connectUsers() {
@@ -147,13 +154,13 @@ function connectUsers() {
         sessionId = url;
 
         stompClient.subscribe('/user/' + sessionId + '/queue/specific-user', function (msgOut) {
-             console.log("RECEIVED FROM THE USER: " + sessionId + " " + msgOut);
-             readSubscribed(msgOut);
+            console.log("RECEIVED FROM THE USER: " + sessionId + " " + msgOut);
+            readSubscribed(msgOut);
         });
 
         stompClient.send("/app/room", {
         }, JSON.stringify({
-            "s":sessionId
+            "s": sessionId
         }));
     });
 }
@@ -170,7 +177,7 @@ function sendName(id) {
         stompClient.send("/app/gameplay", {}, JSON.stringify({"cell":id, "roomId":roomId}));
     else {
         showStatus("NOT YOUR TURN!")
-        setTimeout(() => {hideStatus();}, 1000)
+        setTimeout(() => { hideStatus(); }, 1000)
     }
 }
 
@@ -179,10 +186,10 @@ function readSubscribed(message) {
 
     var response = JSON.parse(message.body);
 
-    if(response.event==EVENT.CONNECT) {
-        if(response.playerOneSessionId!=null)
+    if (response.event == EVENT.CONNECT) {
+        if (response.playerOneSessionId != null)
             processConnectMessage(response);
-    } else if (response.event==EVENT.GAMEPLAY) {
+    } else if (response.event == EVENT.GAMEPLAY) {
         processGameplayMessage(response)
     }
 }
@@ -195,15 +202,15 @@ function processConnectMessage(response) {
     document.getElementById("playerSpan").innerText=(response.startingPlayerName + " starts");
     var myTurn = (sessionId==response.playerOneSessionId);
 
-    lastShootingPlayer=response.playerOneSessionId;
-    currentTurnPlayer=response.playerOneSessionId;
+    lastShootingPlayer = response.playerOneSessionId;
+    currentTurnPlayer = response.playerOneSessionId;
 
-    if(sessionId==response.playerOneSessionId) {
-        response.playerOneShipPositions.forEach(el => document.getElementById("cell_"+el).innerHTML="&#128755;");
+    if (sessionId == response.playerOneSessionId) {
+        response.playerOneShipPositions.forEach(el => document.getElementById("cell_" + el).innerHTML = "&#128755;");
         document.getElementById("name").innerHTML = response.playerOneName;
         console.log(response.playerOneName);
     } else {
-        response.playerTwoShipPositions.forEach(el => document.getElementById("cell_"+el).innerHTML="&#128755;");
+        response.playerTwoShipPositions.forEach(el => document.getElementById("cell_" + el).innerHTML = "&#128755;");
         document.getElementById("name").innerHTML = response.playerTwoName;
         console.log(response.playerTwoName);
     }
@@ -211,46 +218,59 @@ function processConnectMessage(response) {
     highlightBoard(myTurn);
 }
 
+
 function processGameplayMessage(response) {
-    if(response.error != "") {
+    if (response.error != "") {
         console.error(response.error);
         badRequest();
         showStatus(response.error)
-        setTimeout(() => {hideStatus();}, 1000)
+        setTimeout(() => { hideStatus(); }, 1000)
         return;
     }
-    handleReturnedFieldType(response.updatedState, response.cell, lastShootingPlayer==sessionId);
-    lastShootingPlayer=response.currentTurnPlayer;
-    currentTurnPlayer=response.currentTurnPlayer;
-    document.getElementById("playerSpan").innerHTML="Now plays: " + response.currentTurnPlayerName;
 
-    logMove(response.updatedState, response.currentTurnPlayerName, response.cell);
-    var myTurn = (sessionId==response.currentTurnPlayer);
+    var cellArray = response.cells;
+
+    var keys = Object.keys(cellArray);
+
+    if (keys.length > 1) {
+        handleShipShink(cellArray, lastShootingPlayer == sessionId);
+        logMove()
+    } else {
+        handleReturnedFieldType(cellArray[keys[0]], keys[0], lastShootingPlayer == sessionId);
+        logMove(response.updatedState, response.currentTurnPlayerName, response.cell);
+    }
+
+    lastShootingPlayer = response.currentTurnPlayer;
+    currentTurnPlayer = response.currentTurnPlayer;
+    document.getElementById("playerSpan").innerHTML = "Now plays: " + response.currentTurnPlayerName;
+
+
+    var myTurn = (sessionId == response.currentTurnPlayer);
     highlightBoard(myTurn);
 
-    if (response.finished && sessionId==lastShootingPlayer) {
+    if (response.finished && sessionId == lastShootingPlayer) {
         winner();
-    } else if (response.finished && sessionId!=lastShootingPlayer) {
+    } else if (response.finished && sessionId != lastShootingPlayer) {
         lose();
     }
 }
 
 function highlightBoard(currentPlayerTurn) {
-    if(currentPlayerTurn) {
-        document.getElementById("rightSide").className="playerTurn";
+    if (currentPlayerTurn) {
+        document.getElementById("rightSide").className = "playerTurn";
     } else {
-        document.getElementById("rightSide").className="opponentTurn";
+        document.getElementById("rightSide").className = "opponentTurn";
     }
 }
 
 function showStatus(message) {
-    var status=document.getElementById("status");
-    status.innerText=message;
-    status.hidden=false;
+    var status = document.getElementById("status");
+    status.innerText = message;
+    status.hidden = false;
 }
 
 function hideStatus() {
-    document.getElementById("status").hidden=true; 
+    document.getElementById("status").hidden = true;
 }
 
 var moveList = document.getElementById('loglist');
@@ -258,20 +278,20 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  } );
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
 
 function logMove(fieldState, name, cell) {
     var entry = document.createElement('li');
-    if(fieldState === "ACCURATE_SHOT") {
+    if (fieldState === "ACCURATE_SHOT") {
         entry.appendChild(document.createTextNode(getTime() + " Player " + name + " hit a ship on cell " + cell));
     } else {
         entry.appendChild(document.createTextNode(getTime() + " Player " + name + " missed a shot on cell " + cell));
@@ -284,7 +304,7 @@ function getTime() {
     var h = (timeStamp.getHours() < 10 ? '0' : '') + timeStamp.getHours();
     var m = (timeStamp.getMinutes() < 10 ? '0' : '') + timeStamp.getMinutes();
     var s = (timeStamp.getSeconds() < 10 ? '0' : '') + timeStamp.getSeconds();
-    return  h + ":" + m + ":" + s;
+    return h + ":" + m + ":" + s;
 }
 
 createPlayerBoard();
