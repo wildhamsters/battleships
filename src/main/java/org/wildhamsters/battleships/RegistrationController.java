4ecd482e9893b18ff922283cb49c65c1aa64f970
Mariusz Bal <developer.m.info@gmail.com>
2022-05-06 13:58:46 +0200

    Imports fix

--- src/main/java/org/wildhamsters/battleships/RegistrationController.java
+++ src/main/java/org/wildhamsters/battleships/RegistrationController.java
@@ -1,5 +1,7 @@
 package org.wildhamsters.battleships;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
