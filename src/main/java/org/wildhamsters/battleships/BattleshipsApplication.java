27ef9e7f58a550e5fa2a8c40715cf9eeb2e9ded6
Piotr Chowaniec <chowaniec.piotr@poczta.fm>
2022-05-18 11:22:28 +0200

    Remove old database configuration

--- src/main/java/org/wildhamsters/battleships/BattleshipsApplication.java
+++ src/main/java/org/wildhamsters/battleships/BattleshipsApplication.java
@@ -3,8 +3,9 @@ package org.wildhamsters.battleships;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BattleshipsApplication {
 
 	public static void main(String[] args) {
