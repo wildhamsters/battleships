87ccf0f944d2ce1f3fe763d72f35af101a016f40
Piotr Chowaniec <chowaniec.piotr@poczta.fm>
2022-05-20 10:37:42 +0200

    Change ELK IP address

--- src/main/java/org/wildhamsters/battleships/Logger.java
+++ src/main/java/org/wildhamsters/battleships/Logger.java
@@ -9,6 +9,6 @@ class Logger {
         RestTemplate restTemplate = new RestTemplate();
         Log log = new Log(level.toString(), LocalDateTime.now().toString(),
                 "battleShips", className.getName(), msg);
-        restTemplate.postForObject("http://localhost:8080/log", log, Log.class);
+        restTemplate.postForObject("http://64.225.104.111:8000/log", log, Log.class);
     }
 }
